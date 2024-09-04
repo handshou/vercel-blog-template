d196fa9ff3713f7c3f764a0c5781e01217e06f52
hansel <handshou@users.noreply.github.com>
2024-09-04 21:39:11 +0800

    test: update baseurl

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('meta is correct', async ({ page }) => {
-  await page.goto("http://localhost:4321/");
+  await page.goto('/');
 
   await expect(page).toHaveTitle('🎈 hansel');
 });
